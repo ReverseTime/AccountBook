a6bd4dcf9650737964118633aa4515ac1263ae97
Michael <madianyong2201@126.com>
2015-04-12 21:57:23 +0800

    Test

--- src/com/Michael/ArcChartActivity.java
+++ src/com/Michael/ArcChartActivity.java
@@ -3,7 +3,6 @@ package com.Michael;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TextView;
